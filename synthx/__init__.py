6952fd2d6ea1e02f74e21c524244a72ebaf7bbd0
kenki931128 <kenki.nkmr@gmail.com>
2024-03-25 09:45:03 +0800

    add: add a method and a class

--- synthx/__init__.py
+++ synthx/__init__.py
@@ -1,7 +1,9 @@
 """Module: synthx."""
 
 from synthx.core.dataset import Dataset
+from synthx.core.result import SyntheticControlResult
 from synthx.core.sample import sample
+from synthx.method import synthetic_control
 
 
-__all__ = ['Dataset', 'sample']
+__all__ = ['Dataset', 'sample', 'synthetic_control', 'SyntheticControlResult']
