cdfb367196a120bfbbdf4d047fdc03ff3fa8a492
kenki931128 <kenki.nkmr@gmail.com>
2024-03-25 13:12:35 +0800

    add: stats package

--- synthx/__init__.py
+++ synthx/__init__.py
@@ -1,9 +1,16 @@
 """Module: synthx."""
 
+from synthx import stats
 from synthx.core.dataset import Dataset
 from synthx.core.result import SyntheticControlResult
 from synthx.core.sample import sample
 from synthx.method import synthetic_control
 
 
-__all__ = ['Dataset', 'sample', 'synthetic_control', 'SyntheticControlResult']
+__all__ = [
+    'stats',
+    'Dataset',
+    'sample',
+    'synthetic_control',
+    'SyntheticControlResult',
+]
